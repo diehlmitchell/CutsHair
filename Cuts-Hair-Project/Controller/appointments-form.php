d8608b6ae62b1004c617be59e2cbf571570f1a54
Mitchell Diehl <mdiehl@training.local>
2020-11-24 10:35:06 -0500

    added different code for the direction i want to take with "logging in"

--- Cuts-Hair-Project/Controller/appointments-form.php
+++ Cuts-Hair-Project/Controller/appointments-form.php
@@ -2,30 +2,33 @@
 <div class="form-div">
     <div class="form-left">
         <form action="" method="POST">
-        <input type="text" id="name" name="name" placeholder="Name" required class="form-boxs"><br>
-        <select name="service" id="service" required class="form-boxs">
-            <option value="" disabled selected>Select Service</option>
-            <option value="Men's Haircut">Men's Haircut</option>
-            <option value="Women's Haircut">Women's Haircut</option>
-            <option value="Child Haircut">Child Haircut</option>
-            <option value="Hair Color">Hair Color</option>
-            <option value="Hair Wash and dry">Hair Wash and dry</option>
-            <option value="Hair Styling">Hair Styling</option>
-            <option value="Perms">Perms</option>
-            <option value="Facial Hair">Facial Hair</option>
-        </select>
-        <br>
-        <!-- employee -->
-        <select name="employee" id="employee" required class="form-boxs">
-            <option value="" disabled selected>Select Employee</option>
-            <option value="Joy Aveda">Joy Aveda</option>
-            <option value="Alanah Dickson">Alanah Dickson</option>
-            <option value="Salim Rahman">Salim Rahman</option>
-            <option value="Shannen Bassett">Shannen Bassett</option>
-            <option value="Hakim Goulding">Hakim Goulding</option>
-            <option value="Kush Silva">Kush Silva</option>
-        </select>
-        <!-- employee -->
+        
+            <!-- tip and service selection -->
+            <input name="tip" id="tip" type="number" placeholder="Make a tip!" class="form-boxs" min=0> <!-- min of 0 becuase we dont wanna pay nobody to use our business  -->
+                <select name="service" id="service" required class="form-boxs">
+                    <option value="" disabled selected>Select Service</option>
+                    <option value="Men's Haircut">Men's Haircut</option>
+                    <option value="Women's Haircut">Women's Haircut</option>
+                    <option value="Child Haircut">Child Haircut</option>
+                    <option value="Hair Color">Hair Color</option>
+                    <option value="Hair Wash and dry">Hair Wash and dry</option>
+                    <option value="Hair Styling">Hair Styling</option>
+                    <option value="Perms">Perms</option>
+                    <option value="Facial Hair">Facial Hair</option>
+                </select>
+            <!-- name and service selection -->
+            <br>
+            <!-- employees -->
+                <select name="employee" id="employee" required class="form-boxs">
+                    <option value="" disabled selected>Select Employee</option>
+                    <option value="Joy Aveda">Joy Aveda</option>
+                    <option value="Alanah Dickson">Alanah Dickson</option>
+                    <option value="Salim Rahman">Salim Rahman</option>
+                    <option value="Shannen Bassett">Shannen Bassett</option>
+                    <option value="Hakim Goulding">Hakim Goulding</option>
+                    <option value="Kush Silva">Kush Silva</option>
+                </select>
+            <!-- employees -->
     </div>
 
     <div class="form-center">
@@ -40,7 +43,25 @@
         required class="form-boxs">
         <br>
         <input type="submit" value="Submit" class="form-button">
-        <button class="form-button" >Reset</button>
+        <button class="open-button" class ="form-button" onclick="openForm()">Login</button>
+
+        <!-- "Login" popup form -->
+        <div class="form-popup" id="myForm">
+        <form action="" class="form-container">
+            <h1>Login</h1>
+
+            <label for="email"><b>Who are you?</b></label>
+            <select>
+                <option>Mr Poopy Stinky</option>
+                <option>Mr Stinky Poopy</option>
+            </select>
+
+            <button type="submit" class="btnLogin">Login</button>
+            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+        </form>
+        </div>
+
+
     </div>
     </form>
 
@@ -54,5 +75,14 @@
     </div>
 </div>
 
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+</script>
 
 
